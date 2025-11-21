5e6c75e38133ee159b861c97a96521f7a08a15c6
TuxLux40 <info@oliverschlueter.business>
2025-11-21 16:33:42 +0100

    feat(scripts): add base and desktop package installation scripts; remove misc-tools.sh
    feat(flatpak): add backup script for Flatpak repositories and installed applications

---
+++ scripts/pkg-scripts/desktop-packages.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+paru -S --noconfirm --needed \
+    brave-beta-bin \
+    ghostty \
+    google-chrome \
+    lazydocker \
+    lazygit \
+    linutil-bin \
+    microsoft-edge-stable-bin \
+    mission-center \
+    obs-studio \
+    octopi \
+    podman-desktop \
+    proton-mail-bin \
+    proton-pass-bin \
+    protonmail-bridge \
+    steam \
+    visual-studio-code-bin \
+    zed
